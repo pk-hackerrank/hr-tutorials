9b73daeedeb1a9957072e3ccc8bb0de706010f3e
P K Tippa <pktippa@gmail.com>
2016-05-10 12:15:45 +0530

    hr-tutorials 30-days-of-code day-4-class-vs-instance Java, JS impl

--- 30-days-of-code/30-days-of-code.md
+++ 30-days-of-code/30-days-of-code.md
@@ -4,3 +4,4 @@
 2. [Day 1: Data Types](https://www.hackerrank.com/challenges/30-data-types) - [doc](day-1-data-types/day-1-data-types.md) - [NodeJS](day-1-data-types/nodejs/30-data-types.js) - [Java](day-1-data-types/java/src/com/pktippa/DataTypes.java)
 3. [Day 2: Operators](https://www.hackerrank.com/challenges/30-operators) - [doc](day-2-operators/day-2-operators.md) - [Java](day-2-operators/java/src/com/pktippa/Operators.java)
 4. [Day 3: Intro to Conditional Statements](https://www.hackerrank.com/challenges/30-conditional-statements) - [doc](day-3-conditional-statements/day-3-conditional-statements.md) - [Java](day-3-conditional-statements/java/src/com/pktippa/ConditionalStatements.java)
+5. [Day 4: Class vs. Instance](https://www.hackerrank.com/challenges/30-class-vs-instance) - [doc](day-4-class-vs-instance/day-4-class-vs-instance.md) - [Java](day-4-class-vs-instance/java/src/com/pktippa/ClassVsInstance.java) - [NodeJS](day-4-class-vs-instance/nodejs/class-vs-instance.js)
---
+++ 30-days-of-code/day-4-class-vs-instance/java/src/com/pktippa/ClassVsInstance.java
@@ -0,0 +1,54 @@
+package com.pktippa;
+
+import java.util.*;
+
+public class ClassVsInstance {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		ClassVsInstance cvi = new ClassVsInstance();
+		for (int i = 0; i < T; i++) {
+			int age = sc.nextInt();
+			Person p = cvi.new Person(age);
+			p.amIOld();
+			for (int j = 0; j < 3; j++) {
+				p.yearPasses();
+			}
+			p.amIOld();
+			System.out.println();
+		}
+		sc.close();
+	}
+	class Person{
+		private int age;	
+
+		public Person(int initialAge) {
+			if(initialAge<0){
+				System.out.println("Age is not valid, setting age to 0.");
+				this.age = 0;
+			}else{
+				this.age = initialAge;
+			}
+		}
+
+		public void amIOld() {
+			// Write code determining if this person's age is old and print the correct statement:
+			String statement = "";
+			if(age < 13){
+				statement = "You are young.";
+			}else if(age >= 13 && age < 18){
+				statement = "You are teenager.";
+			}else{
+				statement = "You are old.";
+			}
+			System.out.println(statement);
+		}
+
+		public void yearPasses() {
+			// Increment this person's age.
+			this.age++;
+		}
+	}
+
+}
