837852bd8d6629432eab266e4f3423cdbd94eadc
P K Tippa <pktippa@gmail.com>
2016-05-18 12:43:54 +0530

    hr-tutorials 30-days-of-code day-11-2d-arrays nodejs implementation

---
+++ 30-days-of-code/day-11-2d-arrays/nodejs/2darrays.js
@@ -0,0 +1,44 @@
+// Code for reading data from command line input and processing it
+// Start
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', cacheInput).on('end', processData);
+
+function cacheInput(data) {
+    input_stdin += data;
+}
+
+function processData() {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+}
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+// End
+
+function main(){
+	var arr = [];
+    for(arr_i = 0; arr_i < 6; arr_i++){
+       arr[arr_i] = readLine().split(' ');
+       arr[arr_i] = arr[arr_i].map(Number);
+    }
+    var sum = [];
+    var h = 0;
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            sum[h] = arr[i][j] + arr[i][j+1] + arr[i][j+2]
+                    + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1]
+                    + arr[i+2][j+2];
+            h++;
+        }
+    }
+    sum.sort(function(a,b){return a-b;})
+    console.log(sum[15]);
+}
